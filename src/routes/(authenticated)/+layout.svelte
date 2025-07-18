0d4904de2489430b93566cec1a7af05ea35357aa
itsmikesharescode <eviotamikejohnb@gmail.com>
2025-07-19 02:44:18 +0800

    fix file structure

---
+++ src/routes/(authenticated)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import LeftSideBar from '$lib/components/externals/sidebars/left-sidebar/left-sidebar.svelte';
+	import RightSideBar from '$lib/components/externals/sidebars/right-sidebar/right-sidebar.svelte';
+	const { children } = $props();
+</script>
+
+<main>
+	<section class="container mx-auto grid w-full grid-cols-[auto_1fr] xl:grid-cols-[320px_1fr_auto]">
+		<LeftSideBar />
+		{@render children()}
+		<RightSideBar />
+	</section>
+</main>
---
+++ src/routes/(authenticated)/home/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Content from './(components)/(content)/content.svelte';
+	import ContentNav from './(components)/(content-nav)/content-nav.svelte';
+</script>
+
+<aside class="h-[100dvh]">
+	<ContentNav />
+	<Content />
+</aside>
